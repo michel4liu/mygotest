7a1b9bf715d9ae2f0da6e789a2b98a1b33339661
liuhuan <michel4liu@hotmail.com>
2023-07-09 01:35:53 +0800

    阶段提交

--- mytest.go
+++ mytest.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -10,6 +11,9 @@ import (
 var x, y int
 
 func main() {
+
+	const ca, cb int = 1, 2
+
 	var aa = 12345
 
 	fmt.Println(&aa)
@@ -53,4 +57,127 @@ func main() {
 	output.OutputText(ob)
 
 	output.GetTest3()
+
+	// 创建一个新的结构体
+	//fmt.Println(StrutcTest2{"Go 语言", "www.runoob.com", "Go 语言教程", 6495407})
+
+	// // 也可以使用 key => value 格式
+	// fmt.Println(StrutcTest{title: "Go 语言", author: "www.runoob.com", subject: "Go 语言教程", book_id: 6495407})
+
+	// // 忽略的字段为 0 或 空
+	// fmt.Println(StrutcTest{title: "Go 语言", author: "www.runoob.com"})
+
+	//一维数组
+	var arr_1 [5]int
+	fmt.Println(arr_1)
+
+	var arr_2 = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(arr_2)
+
+	arr_3 := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(arr_3)
+
+	arr_4 := [...]int{1, 2, 3, 4, 5, 6}
+	fmt.Println(arr_4)
+
+	arr_5 := [5]int{0: 3, 1: 5, 4: 6}
+	fmt.Println(arr_5)
+
+	//二维数组
+	var arr_6 = [3][5]int{{1, 2, 3, 4, 5}, {9, 8, 7, 6, 5}, {3, 4, 5, 6, 7}}
+	fmt.Println(arr_6)
+
+	arr_7 := [3][5]int{{1, 2, 3, 4, 5}, {9, 8, 7, 6, 5}, {3, 4, 5, 6, 7}}
+	fmt.Println(arr_7)
+
+	arr_8 := [...][5]int{{1, 2, 3, 4, 5}, {9, 8, 7, 6, 5}, {0: 3, 1: 5, 4: 6}}
+	fmt.Println(arr_8)
+
+	var arr = [...]int{1, 2, 3, 4, 5}
+	modifyArr(&arr)
+	fmt.Println(arr)
+
+	var sli_1 []int //nil 切片
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(sli_1), cap(sli_1), sli_1)
+
+	var sli_2 = []int{} //空切片
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(sli_1), cap(sli_2), sli_2)
+
+	var sli_3 = []int{1, 2, 3, 4, 5}
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(sli_3), cap(sli_3), sli_3)
+
+	sli_4 := []int{1, 2, 3, 4, 5}
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(sli_4), cap(sli_4), sli_4)
+
+	var sli_5 []int = make([]int, 5, 8)
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(sli_5), cap(sli_5), sli_5)
+
+	sli_6 := make([]int, 5, 9)
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(sli_6), cap(sli_6), sli_6)
+
+	fmt.Println("------------------------------------------------")
+
+	// sli := [] int {1, 2, 3, 4, 5, 6}
+	// fmt.Printf("len=%d cap=%d slice=%v\n",len(sli),cap(sli),sli)
+
+	// fmt.Println("sli[1] ==", sli[1])
+	// fmt.Println("sli[:] ==", sli[:])
+	// fmt.Println("sli[1:] ==", sli[1:])
+	// fmt.Println("sli[:4] ==", sli[:4])
+
+	// fmt.Println("sli[0:3] ==", sli[0:3])
+	// slix:=sli[0:3]
+	// fmt.Println(cap(slix))
+	// fmt.Printf("len=%d cap=%d slice=%v\n",len(sli[0:3]),cap(sli[0:3]),sli[0:3])
+
+	// fmt.Println("sli[0:3:4] ==", sli[0:3:4])
+	// fmt.Printf("len=%d cap=%d slice=%v\n",len(sli[0:3:4]),cap(sli[0:3:4]),sli[0:3:4])
+
+	fmt.Println("------------------------------------------------")
+	// sli := [] int {4, 5, 6}
+	// fmt.Printf("len=%d cap=%d slice=%v\n",len(sli),cap(sli),sli)
+
+	// sli = append(sli, 7)
+	// fmt.Printf("len=%d cap=%d slice=%v\n",len(sli),cap(sli),sli)
+
+	// sli = append(sli, 8)
+	// fmt.Printf("len=%d cap=%d slice=%v\n",len(sli),cap(sli),sli)
+
+	// sli = append(sli, 9)
+	// fmt.Printf("len=%d cap=%d slice=%v\n",len(sli),cap(sli),sli)
+
+	// sli = append(sli, 10)
+	// fmt.Printf("len=%d cap=%d slice=%v\n",len(sli),cap(sli),sli)
+
+	sli := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(sli), cap(sli), sli)
+
+	//删除尾部 2 个元素
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(sli[:len(sli)-2]), cap(sli[:len(sli)-2]), sli[:len(sli)-2])
+
+	//删除开头 2 个元素
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(sli[2:]), cap(sli[2:]), sli[2:])
+
+	//删除中间 2 个元素
+	sli = append(sli[:3], sli[3+2:]...)
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(sli), cap(sli), sli)
+
+	//匿名结构体
+	p4 := struct {
+		Name string `json:"名称"`
+		Age  int    `json:"-"`
+	}{Name: "匿名", Age: 33}
+	fmt.Println("p4 =", p4)
+
+	//序列化
+	jsons, errs := json.Marshal(p4)
+	if errs != nil {
+		fmt.Println("json marshal error:", errs)
+	}
+	fmt.Println("json data :", string(jsons))
+
+}
+
+func modifyArr(a *[5]int) {
+	a[1] = 20
 }
---
+++ strutcTest/strutcTest.go
@@ -0,0 +1,8 @@
+package strutcTest
+
+type StrutcTest2 struct {
+	Title   string
+	Author  string
+	Subject string
+	Book_id int
+}
